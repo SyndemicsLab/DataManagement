8a06d8646bcc554650f7d11cd1e683262a49d529
Dimitri Baptiste <55843498+ddbaptiste@users.noreply.github.com>
2023-12-11 11:56:43 -0500

    Remove a function added purely for testing

--- include/Configuration.hpp
+++ include/Configuration.hpp
@@ -44,15 +44,6 @@ namespace Data {
         /// @return
         std::shared_ptr<std::string> optional(std::string str);
 
-        std::shared_ptr<std::string> optionalString(std::string str) {
-            boost::optional<std::string> result =
-                this->ptree.get_optional<std::string>(str);
-            if (result.value() != "") {
-                return std::make_shared<std::string>(*result);
-            }
-            return nullptr;
-        }
-
         /// @brief
         /// @param
         /// @return
