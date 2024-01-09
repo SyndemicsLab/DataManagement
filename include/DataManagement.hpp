0b64b627e56abbb9f6a1c6de6a84a5dffd948401
Matthew Carroll <matthew.j.carroll@outlook.com>
2024-01-09 15:36:42 -0500

    round2

---
+++ include/DataManagement.hpp
@@ -0,0 +1,10 @@
+#ifndef DATAMANAGEMENT_HPP_
+#define DATAMANAGEMENT_HPP_
+
+#include "Configuration.hpp"
+#include "DataTable.hpp"
+#include "Reader.hpp"
+#include "SQLite3.hpp"
+#include "Writer.hpp"
+
+#endif
