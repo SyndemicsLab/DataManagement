5ff1900f350ab1e9333b404dc52a7a7323e2fa37
Matthew Carroll <matthew.j.carroll@outlook.com>
2024-01-10 16:02:23 -0500

    I am confused about tests

--- src/Configuration.cpp
+++ src/Configuration.cpp
@@ -1,20 +1,102 @@
 #include "Configuration.hpp"
+#include <boost/lexical_cast.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+
+#include <cassert>
 
 namespace Data {
+    class PTree {
+    public:
+        boost::property_tree::ptree ptree;
+    };
+
+    /// @brief Default template for getting parameters from the config
+    /// @param T Type to return
+    /// @param str String key to search for
+    /// @return The value searched for of type T
+    template <typename T> T Configuration::get(std::string str) {
+        return this->dmTree->ptree.get<T>(str);
+    }
+
+    template int Configuration::get<int>(std::string str);
+    template bool Configuration::get<bool>(std::string str);
+    template float Configuration::get<float>(std::string str);
+    template double Configuration::get<double>(std::string str);
+    template char Configuration::get<char>(std::string str);
+    template std::string Configuration::get<std::string>(std::string str);
+
+    void Configuration::get(
+        std::string str,
+        std::variant<int, bool, float, double, char, std::string> &value) {
+        if (std::holds_alternative<int>(value)) {
+            value = this->get<int>(str);
+        } else if (std::holds_alternative<bool>(value)) {
+            value = this->get<bool>(str);
+        } else if (std::holds_alternative<float>(value)) {
+            value = this->get<float>(str);
+        } else if (std::holds_alternative<double>(value)) {
+            value = this->get<double>(str);
+        } else if (std::holds_alternative<char>(value)) {
+            value = this->get<char>(str);
+        } else if (std::holds_alternative<std::string>(value)) {
+            value = this->get<std::string>(str);
+        }
+    }
+
+    /// @brief Template for the optional parameters
+    /// @param T Type to return
+    /// @param str String key to search for
+    /// @return The optional value searched for of type T
+    template <typename T>
+    std::shared_ptr<T> Configuration::optional(std::string str) {
+        boost::optional<T> result = this->dmTree->ptree.get_optional<T>(str);
+        if (result) {
+            return std::make_shared<T>(*result);
+        }
+        return nullptr;
+    }
+
+    template std::shared_ptr<int> Configuration::optional<int>(std::string str);
+    template std::shared_ptr<bool>
+    Configuration::optional<bool>(std::string str);
+    template std::shared_ptr<float>
+    Configuration::optional<float>(std::string str);
+    template std::shared_ptr<double>
+    Configuration::optional<double>(std::string str);
+    template std::shared_ptr<char>
+    Configuration::optional<char>(std::string str);
+    template std::shared_ptr<std::string>
+    Configuration::optional<std::string>(std::string str);
+
+    void Configuration::optional(
+        std::string str,
+        std::variant<std::shared_ptr<int>, std::shared_ptr<bool>,
+                     std::shared_ptr<float>, std::shared_ptr<double>,
+                     std::shared_ptr<char>, std::shared_ptr<std::string>>
+            &value) {
+
+        if (std::holds_alternative<std::shared_ptr<int>>(value)) {
+            value = this->optional<int>(str);
+        } else if (std::holds_alternative<std::shared_ptr<bool>>(value)) {
+            value = this->optional<bool>(str);
+        } else if (std::holds_alternative<std::shared_ptr<float>>(value)) {
+            value = this->optional<float>(str);
+        } else if (std::holds_alternative<std::shared_ptr<double>>(value)) {
+            value = this->optional<double>(str);
+        } else if (std::holds_alternative<std::shared_ptr<char>>(value)) {
+            value = this->optional<char>(str);
+        } else if (std::holds_alternative<std::shared_ptr<std::string>>(
+                       value)) {
+            value = this->optional<std::string>(str);
+        }
+    }
+
     Configuration::Configuration(std::string configFile) {
         std::ifstream f(configFile.c_str());
         assert((f.good(), "Valid Config File Provided"));
 
-        read_ini(configFile, this->ptree);
-    }
-
-    std::shared_ptr<std::string> Configuration::optional(std::string str) {
-        boost::optional<std::string> result =
-            this->ptree.get_optional<std::string>(str);
-        if (*result != "") {
-            return std::make_shared<std::string>(*result);
-        }
-        return nullptr;
+        read_ini(configFile, this->dmTree->ptree);
     }
 
     std::vector<std::string>
--- src/SQLite3.cpp
+++ src/SQLite3.cpp
@@ -1,6 +1,29 @@
 #include <SQLite3.hpp>
 
+#include <iostream>
+#include <sqlite3.h>
+#include <string>
+#include <unordered_map>
+
 namespace Data {
+    class Database {
+    public:
+        /// @brief Constructor for the class that handles SQLite3 interfacing
+        /// @param database The path to a SQLite3 database
+        Database(std::string database);
+        ~Database();
+
+        /// @brief
+        /// @param
+        SQLTable readTable(std::string query);
+
+    private:
+        // pointer for the sqlite3 connection
+        sqlite3 *db;
+    };
+
+    SQLTable::~SQLTable() { sqlite3_free_table(this->data); }
+
     Database::Database(std::string database) {
         // evaluates as true if an error is raised
         if (sqlite3_open_v2(database.c_str(), &this->db, SQLITE_OPEN_READONLY,
@@ -9,6 +32,8 @@ namespace Data {
             return;
         }
     }
+
+    Database::~Database() { sqlite3_close(this->db); }
 
     SQLTable Database::readTable(std::string query) {
         SQLTable toReturn;
