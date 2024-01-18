d5f194ce8680599fee47f865d481b000e4735247
Matthew Carroll <matthew.j.carroll@outlook.com>
2024-01-18 12:27:37 -0500

    Cmake redo v2 (#38)
    * Maybe this time...
    
    * updates to install and removing bad files
    
    * I'm dumb and forgot to remove the install before adding it to the gitignore
    
    * adding _install to gitignore
    
    * pain
    
    * Maybe, just maybe?
    
    * build release update

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 rm -rf build/*
-cmake -S . build/ -DCMAKE_BUILD_TYPE=Release
-cmake --build build/
-sudo cmake --install build/
+cmake -S . -B build -DBUILD_SHARED_LIBS=YES -DCMAKE_BUILD_TYPE=Release
+cmake --build build
+cmake --install build --prefix _install
