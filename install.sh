7d6f005ad8f265a2b5e4720a111a279542bbed85
Dimitri Baptiste <55843498+ddbaptiste@users.noreply.github.com>
2024-05-09 13:44:22 -0400

    Allow the build path in `install.sh` to be provided as an argument

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+case $1 in
+    "")
+	PREFIX="dminstall"
+	;;
+    *)
+	PREFIX="$1"
+	;;
+esac
+
 rm -rf build/*
 cmake -S . -B build -DBUILD_SHARED_LIBS=YES -DCMAKE_BUILD_TYPE=Release
 cmake --build build
-cmake --install build --prefix dminstall
+cmake --install build --prefix "$PREFIX"
