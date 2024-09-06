331c0a40d70957f35490e227f655b21906210e71
Matthew Carroll <matthew.j.carroll@outlook.com>
2024-09-06 15:54:02 -0400

    Adding Config adjusted implementation

--- include/DataManager.hpp
+++ include/DataManager.hpp
@@ -1,24 +1,33 @@
 #ifndef DATAMANAGER_HPP_
 #define DATAMANAGER_HPP_
 
+#include <any>
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 namespace datamanagement {
+    using Row = std::vector<std::string>;
+    using Table = std::vector<Row>;
     class DataManagerBase {
     private:
         class DataManager;
-        std::unique_ptr<DataManager> pImpl;
+        class Config;
+        std::unique_ptr<DataManager> pImplDM;
+        std::unique_ptr<Config> pImplCF;
 
     public:
         DataManagerBase();
         ~DataManagerBase();
-        int AddCSVTable(std::string const &) const;
-        int Create(std::string const, std::vector<std::string> data) const;
-        int Select(std::string const, std::vector<std::string> data) const;
-        int Update(std::string const, std::vector<std::string> data) const;
-        int Delete(std::string const, std::vector<std::string> data) const;
+        int AddCSVTable(std::string const &filepath) const;
+        int Create(std::string const query, Table data) const;
+        int Select(std::string const query, Table data) const;
+        int Update(std::string const query, Table data) const;
+        int Delete(std::string const query, Table data) const;
+        int GetFromConfig(std::string const key, std::any &data) const;
+        std::vector<std::string>
+        GetConfigSectionCategories(std::string const section) const;
     };
 }; // namespace datamanagement
 
--- src/DataManager.cpp
+++ src/DataManager.cpp
@@ -16,24 +16,30 @@ namespace datamanagement {
         // string is a column)
         static int callback(void *storage, int count, char **data,
                             char **columns) {
-            std::vector<std::string> *vecPtr =
-                (std::vector<std::string> *)storage;
+            Table *vecPtr = (Table *)storage;
+            Row row;
             for (int i = 0; i < count; ++i) {
-                vecPtr->push_back(std::string(data[i]));
+                row.push_back(std::string(data[i]));
             }
+            vecPtr->push_back(row);
             return 0;
         }
 
-        // INCREDIBLY UNSAFE! void* is expected to be a
-        // std::vector<std::string>* because sqlite and C
+        // Be Very Careful! void* is expected to be a Table*
+        // because sqlite and C
         int ExecuteQuery(std::string const query,
                          int (*callback_func)(void *, int, char **, char **),
                          void *data) const {
             char *error_message;
             int rc = sqlite3_exec(db, query.c_str(), callback_func, data,
                                   &error_message);
+            // If there is an error, return the error message in the first index
+            // of the data vector
+            Table *vecPtr = (Table *)data;
+            Row row;
             if (rc != SQLITE_OK) {
-                data = (void *)error_message;
+                row.push_back(std::string(error_message));
+                vecPtr->push_back(row);
             }
             return rc;
         }
@@ -50,27 +56,23 @@ namespace datamanagement {
             std::filesystem::remove("temp.db");
         }
 
-        int Create(std::string const query,
-                   std::vector<std::string> data) const {
+        // CRUD Logical Wrappers
+        int Create(std::string const query, Table data) const {
             return ExecuteQuery(query, NULL, 0);
         }
-
-        int Select(std::string const query,
-                   std::vector<std::string> data) const {
+        int Select(std::string const query, Table data) const {
             return ExecuteQuery(query, callback, &data);
         }
-        int Update(std::string const query,
-                   std::vector<std::string> data) const {
+        int Update(std::string const query, Table data) const {
             return ExecuteQuery(query, callback, &data);
         }
-        int Delete(std::string const query,
-                   std::vector<std::string> data) const {
+        int Delete(std::string const query, Table data) const {
             return ExecuteQuery(query, callback, &data);
         }
     };
 
     DataManagerBase::DataManagerBase() {
-        pImpl = std::make_unique<DataManager>();
+        pImplDM = std::make_unique<DataManager>();
     }
 
     DataManagerBase::~DataManagerBase() = default;
@@ -87,11 +89,13 @@ namespace datamanagement {
         std::vector<std::string> headers;
         std::getline(csv, line);
         std::stringstream s(line);
+
+        // Talk about insecure practices and SQL Injections
         while (std::getline(s, word, ',')) {
+            headers.push_back(word);
             sql += (word + " NOT NULL,");
         }
         sql += ");";
-
         int rc = DataManagerBase::Create(sql, {});
         if (rc != SQLITE_OK) {
             // log error
@@ -99,33 +103,43 @@ namespace datamanagement {
             return;
         }
 
-        std::vector<std::string> row;
+        sql.clear();
+        sql = "INSERT INTO " + table + "(";
+        for (std::string &head : headers) {
+            sql += (head + ",");
+        }
+        sql += ") VALUES ";
+
+        // Talk about insecure practices and SQL Injections
         while (csv >> temp) {
-            row.clear();
+            sql += "(";
             std::getline(csv, line);
             std::stringstream s(line);
             while (std::getline(s, word, ',')) {
-                row.push_back(word);
+                sql += (word + ",");
             }
+            sql += ")";
+        }
+        rc = DataManagerBase::Update(sql, {});
+        if (rc != SQLITE_OK) {
+            // log error
+            csv.close();
+            return;
         }
 
         csv.close();
         return 0;
     }
-    int DataManagerBase::Create(std::string const query,
-                                std::vector<std::string> data) const {
-        return pImpl->Create(query, data);
+    int DataManagerBase::Create(std::string const query, Table data) const {
+        return pImplDM->Create(query, data);
     }
-    int DataManagerBase::Select(std::string const query,
-                                std::vector<std::string> data) const {
-        return pImpl->Select(query, data);
+    int DataManagerBase::Select(std::string const query, Table data) const {
+        return pImplDM->Select(query, data);
     }
-    int DataManagerBase::Update(std::string const query,
-                                std::vector<std::string> data) const {
-        return pImpl->Update(query, data);
+    int DataManagerBase::Update(std::string const query, Table data) const {
+        return pImplDM->Update(query, data);
     }
-    int DataManagerBase::Delete(std::string const query,
-                                std::vector<std::string> data) const {
-        return pImpl->Delete(query, data);
+    int DataManagerBase::Delete(std::string const query, Table data) const {
+        return pImplDM->Delete(query, data);
     }
 } // namespace datamanagement
