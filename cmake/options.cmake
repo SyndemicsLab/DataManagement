512b451afd57f08ec469e7c28cdea0b9bd89f446
Matthew Carroll <28577806+MJC598@users.noreply.github.com>
2025-05-01 08:16:02 -0400

    Adding the code coverage and updating the cmake presets to function correctly

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -4,5 +4,7 @@ option(DATAMANAGEMENT_BUILD_PIC "Build position independent code (-fPIC)" OFF)
 # testing options
 option(DATAMANAGEMENT_BUILD_TESTS "Build tests" OFF)
 
+option(DATAMANAGEMENT_CALCULATE_COVERAGE "Build Coverage Files" OFF)
+
 # install options
 option(DATAMANAGEMENT_INSTALL "Generate the install target" ${DATAMANAGEMENT_MASTER_PROJECT})
