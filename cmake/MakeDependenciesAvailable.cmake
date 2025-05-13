caf52dfac6576aff62428284a9abca26a72c8ae5
Matthew Carroll <28577806+MJC598@users.noreply.github.com>
2025-05-13 11:27:06 -0400

    Forcing the install and exporting of SQLiteCpp

--- cmake/MakeDependenciesAvailable.cmake
+++ cmake/MakeDependenciesAvailable.cmake
@@ -8,12 +8,13 @@ include(cmake/LoadEigen.cmake)
 include(cmake/LoadSpdlog.cmake)
 
 if(DATAMANAGEMENT_BUILD_TESTS STREQUAL "OFF")
-    FetchContent_MakeAvailable(Eigen3 SQLiteCpp Boost spdlog)
+    FetchContent_MakeAvailable(Eigen3 sqlitecpp Boost spdlog)
 elseif(DATAMANAGEMENT_BUILD_TESTS STREQUAL "ON")
     include(cmake/LoadGtest.cmake)
-    FetchContent_MakeAvailable(Eigen3 SQLiteCpp Boost spdlog googletest)
+    FetchContent_MakeAvailable(Eigen3 sqlitecpp Boost spdlog googletest)
     include(GoogleTest)
 endif()
 
 # Thanks SQLiteCpp for being annoying and moving things into the cmake folder
+install(TARGETS SQLiteCpp EXPORT datamanagementTargets)
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_INSTALL_LIBDIR}/cmake)
